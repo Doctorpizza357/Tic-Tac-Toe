6151297a0af32f2e92d9b695c06abc41752a4ce2
Tomas Bentolila <tomasbentolila@gmail.com>
2024-01-19 11:15:48 -0500

    Changed == to null safe .equals

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,6 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
-
+import java.util.Objects;
 
 
 public class TicTacToe {
@@ -58,12 +58,12 @@ public class TicTacToe {
                 tile.addActionListener(e -> {
                     if (gameOver) return;
                     JButton tile1 = (JButton) e.getSource();
-                    if (tile1.getText() == "") {
+                    if (Objects.equals(tile1.getText(), "")) {
                         tile1.setText(currentPlayer);
                         turns++;
                         checkWinner();
                         if (!gameOver) {
-                            currentPlayer = currentPlayer == playerX ? playerO : playerX;
+                            currentPlayer = Objects.equals(currentPlayer, playerX) ? playerO : playerX;
                             textLabel.setText(currentPlayer + "'s turn.");
                         }
                     }
@@ -83,10 +83,10 @@ public class TicTacToe {
      void checkWinner() {
         //horizontal
         for (int r = 0; r < 3; r++) {
-            if (board[r][0].getText() == "") continue;
+            if (Objects.equals(board[r][0].getText(), "")) continue;
 
-            if (board[r][0].getText() == board[r][1].getText() &&
-                    board[r][1].getText() == board[r][2].getText()) {
+            if (Objects.equals(board[r][0].getText(), board[r][1].getText()) &&
+                    Objects.equals(board[r][1].getText(), board[r][2].getText())) {
                 for (int i = 0; i < 3; i++) {
                     setWinner(board[r][i]);
                 }
@@ -97,10 +97,10 @@ public class TicTacToe {
 
         //vertical
         for (int c = 0; c < 3; c++) {
-            if (board[0][c].getText() == "") continue;
+            if (Objects.equals(board[0][c].getText(), "")) continue;
 
-            if (board[0][c].getText() == board[1][c].getText() &&
-                    board[1][c].getText() == board[2][c].getText()) {
+            if (Objects.equals(board[0][c].getText(), board[1][c].getText()) &&
+                    Objects.equals(board[1][c].getText(), board[2][c].getText())) {
                 for (int i = 0; i < 3; i++) {
                     setWinner(board[i][c]);
                 }
@@ -111,9 +111,9 @@ public class TicTacToe {
         }
 
         //diagonally
-        if (board[0][0].getText() == board[1][1].getText() &&
-                board[1][1].getText() == board[2][2].getText() &&
-                board[0][0].getText() != "") {
+        if (Objects.equals(board[0][0].getText(), board[1][1].getText()) &&
+                Objects.equals(board[1][1].getText(), board[2][2].getText()) &&
+                !Objects.equals(board[0][0].getText(), "")) {
             for (int i = 0; i < 3; i++) {
                 setWinner(board[i][i]);
             }
@@ -123,9 +123,9 @@ public class TicTacToe {
         }
 
         //diagonally 2
-        if (board[0][2].getText() == board[1][1].getText() &&
-                board[1][1].getText() == board[2][0].getText() &&
-                board[0][2].getText() != "") {
+        if (Objects.equals(board[0][2].getText(), board[1][1].getText()) &&
+                Objects.equals(board[1][1].getText(), board[2][0].getText()) &&
+                !Objects.equals(board[0][2].getText(), "")) {
             setWinner(board[0][2]);
             setWinner(board[1][1]);
             setWinner(board[2][0]);
